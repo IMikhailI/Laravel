d6e6a6b9a1e51ad319644738ee1a10776e313de6
Михаил <Dobrodomov_mikhail@mail.ru>
2026-09-23 19:31:19 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,27 @@ use App\Http\Controllers\BuildingControllerApi;
 use App\Http\Controllers\RoomControllerApi;
 use App\Http\Controllers\GuestControllerApi;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/rooms', [RoomControllerApi::class, 'index']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+});
 
 Route::get('/buildings', [BuildingControllerApi::class, 'index']);
 Route::get('/buildings/{id}', [BuildingControllerApi::class, 'show']);
 
-Route::get('/rooms', [RoomControllerApi::class, 'index']);
 Route::get('/rooms/{id}', [RoomControllerApi::class, 'show']);
 
 Route::get('/guests', [GuestControllerApi::class, 'index']);
